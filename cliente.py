1d7cdb50d16504154d3ff112636f83c28570d559
Emerson Faria de Oliveira <nosreme20133@hotmail.com>
2026-01-13 20:42:17 -0300

    Starting basic mcpserver with a client connected via stdio

---
+++ cliente.py
@@ -0,0 +1,20 @@
+import asyncio
+from pathlib import Path
+from fastmcp import Client
+
+caminho_servidor = Path(__file__).parent / "server.py"
+cliente = Client(caminho_servidor)
+
+async def testar_servidor(cliente, nome_usuario, id_usuario):
+    async with cliente:
+        argumentos = {"nome_usuario": nome_usuario, "id_usuario": id_usuario}
+        resultado = await cliente.call_tool("dar_bom_dia", arguments=argumentos)
+        print(f"Resultado obtido do servidor MCP: {resultado}")
+
+
+if __name__ == "__main__":
+    asyncio.run(testar_servidor(
+        cliente=cliente,
+        nome_usuario="Emerson",
+        id_usuario="4")
+    )
---
+++ server.py
@@ -0,0 +1,10 @@
+from fastmcp import FastMCP
+
+servidor_mcp = FastMCP("mcp-teste")
+
+@servidor_mcp.tool()
+async def dar_bom_dia(nome_usuario: str, id_usuario: int) -> str:
+    return f'Olá, {nome_usuario}! (ID {id_usuario})'
+
+if __name__ == '__main__':
+    servidor_mcp.run(transport="stdio")
