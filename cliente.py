d59108eb71f8ddd72b43e2d16a784fb4467618e7
Emerson Faria de Oliveira <nosreme20133@hotmail.com>
2026-01-15 22:07:10 -0300

    Integration of weather MCP server with claude!

--- cliente.py
+++ cliente.py
@@ -9,19 +9,20 @@ caminho_servidor = 'http://localhost:8000/sse'
 cliente = Client(caminho_servidor)
 
 
-async def testar_servidor(cliente, busca):
+async def testar_servidor(cliente, local):
     dotenv.load_dotenv()
     api_key = os.getenv("OPENAI_API_KEY")
 
     async with cliente:
-        argumentos = {"busca": busca}
-        resultado = await cliente.call_tool("buscar_wikipedia", arguments=argumentos)
-        print(resultado)
+        argumentos = {"local": local}
+        tempo_atual = await cliente.call_tool("buscar_tempo_atual", arguments=argumentos)
+        previsao_tempo = await cliente.call_tool("buscar_previsao_tempo", arguments=argumentos)
 
         mensagem_sistema = f"""
-        Voce é um bot que faz busca na wikipedia, o usuario buscou pelo 
-        seguinte tema: {busca}. 
-        Para esta busca, voce recebeu a seguinte responsta: {resultado} 
+        Voce é um bot que faz buscas de previsão do tempo e sintetiza a resposta  
+        O usuário buscou pela previsão no seguinte local: {local}. 
+        Para esta busca, voce recebeu a seguinte previsão: {previsao_tempo}
+        Alem disso o tempo atual é {tempo_atual} 
         Com base neste conteudo formate uma resposta amigavel ao usuário
         """
         cliente_openai = OpenAI(api_key=api_key)
@@ -29,12 +30,12 @@ async def testar_servidor(cliente, busca):
         response = cliente_openai.responses.create(
             model='gpt-4o-mini',
             instructions=mensagem_sistema,
-            input="Pode me falar mais sobre este assunto?"
+            input="Qual a previsão do tempo no local indicado?"
         )
         print(response.output_text)
 
 if __name__ == "__main__":
     asyncio.run(testar_servidor(
         cliente=cliente,
-        busca='Isaac Asimov')
+        local='Ouro Fino')
     )
